b3a24067025feb9b61ea8a18b5373eba05cf8bdf
Juvencio Linhares <jlinhares@cpqi.com>
2022-07-19 23:54:13 -0300

    Generics Exercicio Reuso

---
+++ GenericsExercicioReuso/GenericsExercicioReuso/PrintService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace GenericsExercicioReuso
+{
+    /*toda a classe PrintService é de um tipo generico específico do tipo T.
+     * que pode ser qualquer outra letra,
+     * o importante é manter um padrão.
+     */
+    internal class PrintService<T> //parametrizado com o tipo T
+    {
+        //vetor p armazenar 10 numeros inteiros:
+        private T[] _values = new T[10];
+
+        //contar quantos numeros foram add
+        private int _count = 0;
+
+        public void addValue(T value)
+        {
+
+            if (_count == 10)//programação de defesa
+            {
+                throw new InvalidOperationException("PrintService is full!");
+            }
+            _values[_count] = value;//_value na posiçao count = value
+            _count++;
+
+        }
+        public T first()
+        {
+            if (_count == 0)//programação de defesa
+            {
+                throw new InvalidOperationException("PrintService is empty!");
+            }
+
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)//impriminto até a penultima posição do vetor
+            {
+                Console.Write(_values[i] + ", ");
+            }
+
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ GenericsExercicioReuso/GenericsExercicioReuso/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace GenericsExercicioReuso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vou criar um programa que mexe com o tipo que eu quiser 
+            PrintService<int> printService = new PrintService<int>();
+            Console.WriteLine("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.addValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.first());
+        }
+    }
+}
+/*discussão a respeito do generic :
+ * se eu quiser um programa que posso aceitar int ou string:
+ * 1- solução: criar uma classe printservice recebendo strings em vez de int(funciona, mas não há reuso)
+ * 2- solução: usar o object no lugar do int: funciona pro reuso, mas nao tem o type safety e tem problema de performance
+ * type safety(tipo salvo): se eu colocar tudo como object eu posso colocar o programa pra somar string com int,
+ * double com char etc, por que tudo é object, então usar object não é aconselhavel, vai dar um problema em tempo de execução.
+ * isso ainda faz perder performance por causa de boxing e unboxing
+ * 
+ * solução ideal: ...<T> => parametrização por tipo, vou declara uma variavel t e na hora de declarar o objeto
+ * eu vou especificar qual tipo eu quero.
+ */
