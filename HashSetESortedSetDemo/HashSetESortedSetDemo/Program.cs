156a929fcf93330a78f8e8a91fff4db780f109bc
Juvencio Linhares <jlinhares@cpqi.com>
2022-07-20 13:27:55 -0300

    HashSet E SortedSet Demo

---
+++ HashSetESortedSetDemo/HashSetESortedSetDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetESortedSetDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instancia de um conjunto(importa o System.Collections.Generic;)
+            HashSet<string> set = new HashSet<String>();
+
+            //inserir elementos:
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            //teste está contido:
+            Console.WriteLine(set.Contains("Notebook"));
+           
+            //imprimir o conjunto:
+            foreach(String p in set)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
