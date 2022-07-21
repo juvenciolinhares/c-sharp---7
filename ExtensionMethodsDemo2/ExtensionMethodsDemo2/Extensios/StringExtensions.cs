1a55f67e7067e16814a06e7921b057121d1e5eff
Juvencio Linhares <jlinhares@cpqi.com>
2022-07-20 23:51:19 -0300

    Extension Methods Demo2

---
+++ ExtensionMethodsDemo2/ExtensionMethodsDemo2/Extensios/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    static class StringExtensions
+    {
+        
+        public static string Cut(this string ThisObj, int count)//n precisa ser, obrigatoriamente ThisObj, pode ser outro, o importante é a referencia
+        {
+            //metodo p recortar o string:
+
+            if(ThisObj.Length <= count)//se o string original ja tiver do tamanho que eu quero cortar, n vou precisar cortar
+            {
+                return ThisObj;
+            }
+            else
+            {
+                return ThisObj.Substring(0, count) + "...";
+            }
+        }
+    }
+}
---
+++ ExtensionMethodsDemo2/ExtensionMethodsDemo2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace ExtensionMethodsDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s1 = "Good mornig dear students!";
+            Console.WriteLine(s1.Cut(10));
+
+        }
+    }
+}
+/*
+ * criar um extension method chamado Cut(int) que vai gerar um recorte no tamanho original daquele tamanho:
+ */
