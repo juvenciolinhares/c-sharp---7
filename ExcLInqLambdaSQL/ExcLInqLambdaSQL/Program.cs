bc9d111fd224d3328c3c2bf773c62b1a470b3ad4
Juvencio Linhares <jlinhares@cpqi.com>
2022-07-25 22:57:02 -0300

    Exercicio Linq Lambda SQL 2

---
+++ ExcLInqLambdaSQL/ExcLInqLambdaSQL/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using ExcLInqLambdaSQL.Entities;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+namespace ExcLInqLambdaSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Console.Write("Enter full file path");
+             string path = Console.ReadLine();
+
+             //instanciar a lista de prods
+             List<Product> list = new List<Product>();
+
+             //ler o arquivo do path instanciando novos produtos e inserindo na lista:
+
+             using (StreamReader sr = File.OpenText(path))
+             {
+                 while (!sr.EndOfStream)
+                 {
+                     string[] fields = sr.ReadLine().Split(',');
+                     string name = fields[0];
+                     double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                     list.Add(new Product(name, price));
+                 }
+             }
+             //encontrar o preço médio dos produtos:
+             //1- transformar a lista de produtos em uma lista de preços(double)
+             var avarage = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();//DefaultIfEmpty(0.0): evitar o prog quebrar, caso lista vazia
+             Console.WriteLine("Avarage price = " + avarage.ToString("F2",CultureInfo.InvariantCulture));
+
+             //mostrar nomes dos prod abaixo da média ordenados em ordem alfab decresente
+             var names = list.Where(p => p.Price < avarage).OrderByDescending(p => p.Name).Select(p => p.Name);
+
+             //mostrar p resultado:
+             foreach(string name in names)
+             {
+                 Console.WriteLine(name);
+             }
+            */
+            Console.WriteLine("enter full file path");
+            string path = Console.ReadLine();
+
+            using(StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                }
+            }
+
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+
+        }
+    }
+}
---
+++ ExcLinqLambdaSQL2/ExcLinqLambdaSQL2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using ExcLinqLambdaSQL2.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.IO;
+
+namespace ExcLinqLambdaSQL2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+            Console.Write("Enter salary: ");
+            double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            List<Employee> list = new List<Employee>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+
+                        list.Add(new Employee(name, email, salary));
+                    }
+                    var emails = list.Where(p => p.Salary > limit).OrderBy(x => x.Email).Select(x => x.Email);
+                    var sum = list.Where(x => x.Name[0] == 'M').Sum(p => p.Salary);
+
+                    Console.WriteLine("Email of people whose salary is more than " + limit.ToString("F2", CultureInfo.InvariantCulture));
+                    foreach (string e in emails)
+                    {
+                        Console.WriteLine(e);
+                    }
+
+
+                    Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
+
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+/*
+ * ler dados de um func(nome, email, salario) em um arquivo .csv
+ * 
+ * mostrar em ordem alfabetica o email do func com salario superior a um valor 2 fornecido pelo usuario
+ * 
+ * mostrar soma dos salarios dos func começados com a letra m
+ * 
+ * C:\testeLinqLambdaSQL2\in.txt
+ */
+
